d88123c19be0e1c62df227fdba3828872b929a39
dumbesthuman <riteshhiremath2004@gmail.com>
2025-06-30 01:04:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll('.tab');
+    const sections = document.querySelectorAll('.tab-section');
+
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        sections.forEach(s => s.classList.remove('active'));
+
+        tab.classList.add('active');
+        const selected = tab.getAttribute('data-tab');
+        document.getElementById(selected).classList.add('active');
+      });
+    });
+
